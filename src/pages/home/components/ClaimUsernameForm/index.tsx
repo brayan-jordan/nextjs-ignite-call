039881680c239676492ba1e7a2a99f495003641f
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2024-01-28 07:45:47 -0300

    feat: Configurando e utilizando react-hook-form para lidar com formulários da aplicação.

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,19 +1,57 @@
-import { Button, TextInput } from '@design-system-experience/react'
-import { FormContainer } from './styles'
+import { Button, Text, TextInput } from '@design-system-experience/react'
+import { FormAnnotation, FormContainer } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormProps = z.infer<typeof claimUsernameFormSchema>
 
 export function ClainUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormProps>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormProps) {
+    console.log(data)
+  }
+
   return (
-    <FormContainer as="form">
-      <TextInput
-        containerProps={{ size: 'sm' }}
-        prefix="ignite.com/"
-        placeholder="seu-usuário"
-      />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </FormContainer>
+    <>
+      <FormContainer as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          containerProps={{ size: 'sm' }}
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </FormContainer>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
